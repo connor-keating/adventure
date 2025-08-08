88b48532fa3aaa3cd1011c2d97a33e7db82f16fa
Connor <conkeating32@gmail.com>
2025-08-08 15:03:18 -0400

    Rendering a quad with index arrays.

--- src/render_opengl.cpp
+++ src/render_opengl.cpp
@@ -172,10 +172,12 @@ static void APIENTRY gl_debug_callback(GLenum source, GLenum type, GLuint id, GL
     i32 char_written_count = sprintf_s(sentence_buffer, sizeof(sentence_buffer), "OpenGL Error: %s \033[0m", message);
     ASSERT(false, sentence_buffer);
   }
+  /*
   else
   {
     printf("OpenGL Error: %s\n", message);
   }
+  */
 }
 
 #endif
@@ -361,10 +363,24 @@ void point_setup(arena *scratch, render_program *prog)
 
 void tri_setup(arena *scratch, render_program *prog)
 {
+  /*
   f32 verts[] = {
     -0.5f, -0.5f, 0.0f, // left
      0.5f, -0.5f, 0.0f, // right
      0.0f,  0.5f, 0.0f, // top
+  };
+  */
+
+  f32 verts[] = {
+    // positions      
+     0.5f,  0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+    -0.5f, -0.5f, 0.0f,
+    -0.5f,  0.5f, 0.0f,
+  };
+  unsigned int indices[] = {  
+    0, 1, 3, // first triangle
+    1, 2, 3  // second triangle
   };
   // Set up vertex attribute
   glGenVertexArrays(1, &prog->vao);
@@ -378,6 +394,11 @@ void tri_setup(arena *scratch, render_program *prog)
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(verts[0]), (void*)0);
   glEnableVertexAttribArray(0);
   glBindBuffer(GL_ARRAY_BUFFER, 0);
+  // Set up EBO
+  GLuint ebo;
+  glGenBuffers(1, &ebo);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
   // Shader program
   size_t byte_count;
   // Vertx shader source
@@ -428,7 +449,8 @@ void draw_triangles(render_program *prog)
   glUseProgram(prog->shader_program);
   glBindVertexArray(prog->vao);
   // Draw the point
-  glDrawArrays(GL_TRIANGLES, 0, 3);
+  // glDrawArrays(GL_TRIANGLES, 0, 3);
+  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 }
 
   
