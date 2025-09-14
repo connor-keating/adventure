79bbdbfc44f5cb38667e7edba56019ad1b2109d8
Connor <conkeating32@gmail.com>
2025-09-14 12:39:06 -0400

    Finding font file.

--- src/text.cpp
+++ src/text.cpp
@@ -6,4 +6,8 @@
 
 void text_atlas_init()
 {
+  const char *font_file = "C:\\WINDOWS\\Fonts\\arial.ttf";
+  int answer = file_exists(font_file);
+  ASSERT(answer == 1, "Font file not found.");
+  printf("Done\n");
 }
