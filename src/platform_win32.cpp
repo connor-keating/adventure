7273bff9ff1272a2cba1289a8aaff93f086681a4
Connor <conkeating32@gmail.com>
2025-10-25 17:56:18 -0400

    Platform implementation now defines an internal state variable for it to manage in the global space. That's whats used in the init and show function.

--- src/platform_win32.cpp
+++ src/platform_win32.cpp
@@ -7,13 +7,10 @@
 #include "tinyobj_loader_c.h"
 
 
-
-struct platform_window
+struct platform_state
 {
   HWND handle;
   HINSTANCE instance;
-  f32 width;
-  f32 height;
 };
 
 enum control_bindings
@@ -31,6 +28,10 @@ enum control_state
   CONTROL_DOWN,
   CONTROL_RELEASED,
 };
+
+
+// Internal global state
+global platform_state state;
 
 
 internal LRESULT CALLBACK win32_message_callback(HWND window_handle, UINT message_id, WPARAM param_w, LPARAM param_l)
@@ -109,6 +110,91 @@ internal void get_file_data(void* ctx, const char* filename, const int is_mtl, c
 void platform_status()
 {
   printf("Library working\n");
+}
+
+
+platform_window platform_window_init()
+{
+  platform_window wind = {};
+  state.instance = GetModuleHandleA(0);
+  i32 window_x = 50;
+  i32 window_y = 90;
+  i32 window_w = 50;
+  i32 window_h = 50;
+  const char *window_name = "Renderer";
+  // Create win32 window class.
+  WNDCLASSEXA window_class = {};
+  window_class.cbSize = sizeof(window_class);
+  window_class.style = CS_HREDRAW|CS_VREDRAW; // |CS_OWNDC;
+  window_class.lpfnWndProc = win32_message_callback;
+  window_class.hInstance = state.instance;
+  window_class.hCursor = LoadCursor(0, IDC_ARROW);
+  // window_class.hbrBackground = CreateSolidBrush(RGB(255, 0, 255)); // Magenta background
+  window_class.hbrBackground = (HBRUSH)COLOR_WINDOW;
+  window_class.lpszClassName = "WINDOW_CLASS";
+  ATOM window_id = RegisterClassExA(&window_class);
+  ASSERT(window_id, "ERROR: Failed to register window.");
+  
+  // Create the window we described.
+  state.handle = CreateWindowExA(
+    WS_EX_CLIENTEDGE,            // style_extended: has list of possible values.    
+    window_class.lpszClassName,  // class_name: null-terminated string.         
+    window_name,                  // name: string window name to display in title bar.              
+    WS_OVERLAPPEDWINDOW,        // style_basic: has list of possible values.        
+    window_x,                          // position_x: Horizontal window position      
+    window_y,                          // position_y: Vertical window position         
+    window_w,                          // width: Window width in screen coordinates
+    window_h,                          // height: Window height in screen coordinates
+    0,                          // window_parent: Handle to the parent window.
+    0,                          // window_menu: Optional child window ID.
+    state.instance,      // window_handle: handle to this window's module.
+    0                          // window_data_pointer: Pointer to CREATESTRUCT var that sends a message to the window.
+  );
+  ASSERT(state.handle, "ERROR: Failed to create window.");
+
+  // Get monitor info and resize the window.
+  MONITORINFO monitor_info = {};
+  monitor_info.cbSize = sizeof(MONITORINFO);
+  HMONITOR monitor_handle = MonitorFromWindow(state.handle, MONITOR_DEFAULTTOPRIMARY);
+  BOOL monitor_info_success = GetMonitorInfoA(monitor_handle, &monitor_info);
+  ASSERT(monitor_info_success, "ERROR: Failed to get monitor info.");
+  i32 monitor_width  = monitor_info.rcMonitor.right - monitor_info.rcMonitor.left;
+  i32 monitor_height = monitor_info.rcMonitor.bottom - monitor_info.rcMonitor.top;
+  // Calculate our desired window dimensions.
+  // Get DPI scaling factor
+  u32 dpi = GetDpiForWindow(state.handle);
+  f32 scale_factor = dpi / 96.0f; // Scaling factor (1.0 at 96 DPI, 1.5 at 144 DPI, etc.)
+  i32 client_w = MulDiv(monitor_width, 1, 2 * scale_factor);
+  i32 client_h = MulDiv(monitor_height, 1, 2 * scale_factor);
+  RECT window_border = {0, 0, client_w, client_h};
+  AdjustWindowRectExForDpi(&window_border, WS_OVERLAPPEDWINDOW, FALSE, 0, dpi);
+  window_w = window_border.right - window_border.left;
+  window_h = window_border.bottom - window_border.top;
+  window_x = (monitor_width  - window_w) / 2;
+  window_y = (monitor_height - window_h) / 2;
+  BOOL window_success = SetWindowPos(
+    state.handle,
+    HWND_TOP,
+    window_x,
+    window_y,
+    window_w,
+    window_h,
+    0
+  );
+  ASSERT(window_success, "Failed to resize and open window.");
+  wind.is_running = true;
+  wind.width = window_w;
+  wind.height = window_h;
+  wind.state = &state;
+  return wind;
+}
+
+
+void platform_window_show()
+{
+  i32 display_flags = SW_SHOW;
+  ShowWindow(state.handle, display_flags);
+  UpdateWindow(state.handle);
 }
 
 
@@ -257,82 +343,11 @@ void message_process(HWND handle, i32 *input_map, control_state *input_state)
 }
 
 
-void window_init(platform_window *wind)
-{
-  wind->instance = GetModuleHandleA(0);
-  i32 window_x = 50;
-  i32 window_y = 90;
-  i32 window_w = 50;
-  i32 window_h = 50;
-  const char *window_name = "Renderer";
-  // Create win32 window class.
-  WNDCLASSEXA window_class = {};
-  window_class.cbSize = sizeof(window_class);
-  window_class.style = CS_HREDRAW|CS_VREDRAW; // |CS_OWNDC;
-  window_class.lpfnWndProc = win32_message_callback;
-  window_class.hInstance = wind->instance;
-  window_class.hCursor = LoadCursor(0, IDC_ARROW);
-  // window_class.hbrBackground = CreateSolidBrush(RGB(255, 0, 255)); // Magenta background
-  window_class.hbrBackground = (HBRUSH)COLOR_WINDOW;
-  window_class.lpszClassName = "WINDOW_CLASS";
-  ATOM window_id = RegisterClassExA(&window_class);
-  ASSERT(window_id, "ERROR: Failed to register window.");
-  
-  // Create the window we described.
-  wind->handle = CreateWindowExA(
-    WS_EX_CLIENTEDGE,            // style_extended: has list of possible values.    
-    window_class.lpszClassName,  // class_name: null-terminated string.         
-    window_name,                  // name: string window name to display in title bar.              
-    WS_OVERLAPPEDWINDOW,        // style_basic: has list of possible values.        
-    window_x,                          // position_x: Horizontal window position      
-    window_y,                          // position_y: Vertical window position         
-    window_w,                          // width: Window width in screen coordinates
-    window_h,                          // height: Window height in screen coordinates
-    0,                          // window_parent: Handle to the parent window.
-    0,                          // window_menu: Optional child window ID.
-    wind->instance,      // window_handle: handle to this window's module.
-    0                          // window_data_pointer: Pointer to CREATESTRUCT var that sends a message to the window.
-  );
-  ASSERT(wind->handle, "ERROR: Failed to create window.");
-
-  // Get monitor info and resize the window.
-  MONITORINFO monitor_info = {};
-  monitor_info.cbSize = sizeof(MONITORINFO);
-  HMONITOR monitor_handle = MonitorFromWindow(wind->handle, MONITOR_DEFAULTTOPRIMARY);
-  BOOL monitor_info_success = GetMonitorInfoA(monitor_handle, &monitor_info);
-  ASSERT(monitor_info_success, "ERROR: Failed to get monitor info.");
-  i32 monitor_width  = monitor_info.rcMonitor.right - monitor_info.rcMonitor.left;
-  i32 monitor_height = monitor_info.rcMonitor.bottom - monitor_info.rcMonitor.top;
-  // Calculate our desired window dimensions.
-  // Get DPI scaling factor
-  u32 dpi = GetDpiForWindow(wind->handle);
-  f32 scale_factor = dpi / 96.0f; // Scaling factor (1.0 at 96 DPI, 1.5 at 144 DPI, etc.)
-  i32 client_w = MulDiv(monitor_width, 1, 2 * scale_factor);
-  i32 client_h = MulDiv(monitor_height, 1, 2 * scale_factor);
-  RECT window_border = {0, 0, client_w, client_h};
-  AdjustWindowRectExForDpi(&window_border, WS_OVERLAPPEDWINDOW, FALSE, 0, dpi);
-  window_w = window_border.right - window_border.left;
-  window_h = window_border.bottom - window_border.top;
-  window_x = (monitor_width  - window_w) / 2;
-  window_y = (monitor_height - window_h) / 2;
-  BOOL window_success = SetWindowPos(
-    wind->handle,
-    HWND_TOP,
-    window_x,
-    window_y,
-    window_w,
-    window_h,
-    0
-  );
-  ASSERT(window_success, "Failed to resize and open window.");
-}
-
-
 void window_size_get(platform_window *wind)
 {
   // Get window dimension
   RECT size;
-  GetClientRect(wind->handle, &size);
+  GetClientRect(state.handle, &size);
   wind->width  = size.right;
   wind->height = size.bottom;
   if ((wind->width == 0) && (wind->height == 0))
