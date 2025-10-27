f014494af664d26cd1dc56900b6a1eb03a73e7eb
Connor <conkeating32@gmail.com>
2025-10-27 10:13:21 -0400

    Moving all header stuff out of OpenGL implementation file. Temporarily turning off most of the file while porting things to platform lib. Moving frame init and frame end funcs to the top. Using platform swapbuffers.

--- src/render_opengl.cpp
+++ src/render_opengl.cpp
@@ -1,5 +1,6 @@
-#include <windows.h>
-#include <gl/GL.h>
+
+#include "platform.h"
+#include "opengl.h"
 
 /*
 YOU MUST USE SETUP A VBO BEFORE YOU CALL glVertexAttribPointer() 
@@ -8,7 +9,6 @@ OTHERWISE YOU'LL CRASH.
 
 struct render_state
 {
-  HDC context;
   f32 width;
   f32 height;
 };
@@ -19,162 +19,6 @@ struct render_buffer
   u32 vao;
   u32 ebo;
 };
-
-typedef char        GLchar;
-typedef ptrdiff_t   GLsizeiptr;
-typedef ptrdiff_t   GLintptr;
-
-
-#pragma region OpenGL Extensions
-#define GL_DEBUG_OUTPUT_SYNCHRONOUS       0x8242
-#define GL_DEBUG_OUTPUT                   0x92E0
-#define GL_VERTEX_SHADER                  0x8B31
-#define GL_COMPILE_STATUS                 0x8B81
-#define GL_FRAGMENT_SHADER                0x8B30
-#define GL_LINK_STATUS                    0x8B82
-#define GL_ARRAY_BUFFER                   0x8892
-#define GL_STATIC_DRAW                    0x88E4
-#define GL_DYNAMIC_DRAW                   0x88E8
-#define GL_ELEMENT_ARRAY_BUFFER           0x8893
-#define GL_CLAMP_TO_EDGE                  0x812F
-#define GL_TEXTURE0                       0x84C0
-#define GL_TEXTURE1                       0x84C1
-#define GL_TEXTURE2                       0x84C2
-#define GL_TEXTURE3                       0x84C3
-#define GL_TEXTURE4                       0x84C4
-#define GL_TEXTURE5                       0x84C5
-#define GL_TEXTURE6                       0x84C6
-#define GL_TEXTURE7                       0x84C7
-#define GL_TEXTURE8                       0x84C8
-#define GL_TEXTURE9                       0x84C9
-#define GL_TEXTURE10                      0x84CA
-#define GL_TEXTURE11                      0x84CB
-#define GL_TEXTURE12                      0x84CC
-#define GL_TEXTURE13                      0x84CD
-#define GL_TEXTURE14                      0x84CE
-#define GL_TEXTURE15                      0x84CF
-#define WGL_DRAW_TO_WINDOW_ARB            0x2001
-#define WGL_SUPPORT_OPENGL_ARB            0x2010
-#define WGL_DOUBLE_BUFFER_ARB             0x2011
-#define WGL_SWAP_METHOD_ARB               0x2007
-#define WGL_PIXEL_TYPE_ARB                0x2013
-#define WGL_ACCELERATION_ARB              0x2003
-#define WGL_COLOR_BITS_ARB                0x2014
-#define WGL_ALPHA_BITS_ARB                0x201B
-#define WGL_DEPTH_BITS_ARB                0x2022
-#define WGL_FULL_ACCELERATION_ARB         0x2027
-#define WGL_SWAP_COPY_ARB                 0x2029
-#define WGL_TYPE_RGBA_ARB                 0x202B
-#define WGL_CONTEXT_MAJOR_VERSION_ARB     0x2091
-#define WGL_CONTEXT_MINOR_VERSION_ARB     0x2092
-#define WGL_CONTEXT_PROFILE_MASK_ARB      0x9126
-#define WGL_CONTEXT_FLAGS_ARB             0x2094
-#define WGL_CONTEXT_DEBUG_BIT_ARB         0x00000001
-#define WGL_CONTEXT_CORE_PROFILE_BIT_ARB  0x00000001
-#define GL_DEBUG_SEVERITY_HIGH            0x9146
-#define GL_DEBUG_SEVERITY_MEDIUM          0x9147
-#define GL_DEBUG_SEVERITY_LOW             0x9148
-#define GL_DEBUG_SOURCE_APPLICATION       0x824A
-#define GL_DEBUG_TYPE_ERROR               0x824C
-#define GL_PROGRAM_POINT_SIZE             0x8642
-#define GL_R8                             0x8229
-#define GL_SHADER_STORAGE_BUFFER          0x90D2
-#define GL_TEXTURE_3D                     0x806F
-#define GL_TEXTURE_WRAP_R                 0x8072
-#define GL_MAX_3D_TEXTURE_SIZE            0x8073
-#pragma endregion
-
-
-#pragma region OpenGL Functions
-// Unique windows functions not supposed to be loaded with the others.
-typedef BOOL WINAPI glfunc_wglChoosePixelFormatARB(HDC hdc, const int *piAttribIList, const FLOAT *pfAttribFList, UINT nMaxFormats, int *piFormats, UINT *nNumFormats);
-typedef HGLRC WINAPI glfunc_wglCreateContextAttribsARB (HDC hDC, HGLRC hShareContext, const int *attribList);
-
-
-typedef void (APIENTRY  *GLDEBUGPROC)(GLenum source,GLenum type,GLuint id,GLenum severity,GLsizei length,const GLchar *message,const void *userParam);
-
-#define GL_FUNC_SIGNATURE(ret, name, ...) typedef ret WINAPI glfunc_##name(__VA_ARGS__);
-#define REQUIRED_OPENGL_FUNCTIONS                                                                                                                           \
-    GL_FUNC_SIGNATURE(GLuint, glCreateProgram, void)                                                                                                        \
-    GL_FUNC_SIGNATURE(GLuint, glCreateShader, GLenum shaderType)                                                                                            \
-    GL_FUNC_SIGNATURE(bool, wglSwapIntervalEXT, int interval)                                                                                               \
-    GL_FUNC_SIGNATURE(int,  wglGetSwapIntervalEXT, void)                                                                                                \
-    GL_FUNC_SIGNATURE(void, glUniformMatrix4fv, GLint location, GLsizei count, GLboolean transpose, const GLfloat *value)                                   \
-    GL_FUNC_SIGNATURE(void, glUniform3fv, GLint location, GLsizei count, const GLfloat *value)                                                              \
-    GL_FUNC_SIGNATURE(void, glCompileShader, GLuint shader)                                                                                                 \
-    GL_FUNC_SIGNATURE(void, glShaderSource, GLuint shader, GLsizei count, const GLchar** string, const GLint *length)                                       \
-    GL_FUNC_SIGNATURE(void, glGetShaderiv, GLuint shader, GLenum pname, GLint* params)                                                                      \
-    GL_FUNC_SIGNATURE(void, glGetShaderInfoLog, GLuint shader, GLsizei maxLength, GLsizei* length, char *infoLog)                                           \
-    GL_FUNC_SIGNATURE(void, glAttachShader, GLuint program, GLuint shader)                                                                                  \
-    GL_FUNC_SIGNATURE(void, glDetachShader, GLuint program, GLuint shader)                                                                                  \
-    GL_FUNC_SIGNATURE(void, glLinkProgram, GLuint program)                                                                                                  \
-    GL_FUNC_SIGNATURE(void, glGetProgramiv, GLuint program, GLenum pname, GLint *params)                                                                    \
-    GL_FUNC_SIGNATURE(void, glDeleteShader, GLuint shader)                                                                                                  \
-    GL_FUNC_SIGNATURE(void, glGenVertexArrays, GLsizei n, GLuint *arrays)                                                                                   \
-    GL_FUNC_SIGNATURE(void, glGenBuffers, GLsizei n, GLuint* buffers)                                                                                       \
-    GL_FUNC_SIGNATURE(void, glBindVertexArray, GLuint array)                                                                                                \
-    GL_FUNC_SIGNATURE(void, glBindBuffer, GLenum target, GLuint buffer)                                                                                     \
-    GL_FUNC_SIGNATURE(void, glBufferData, GLenum target, GLsizeiptr size, const GLvoid* data, GLenum usage)                                                 \
-    GL_FUNC_SIGNATURE(void, glVertexAttribPointer, GLuint index, GLint size, GLenum type, GLboolean normalized, GLsizei stride, const GLvoid* pointer)      \
-    GL_FUNC_SIGNATURE(void, glEnableVertexAttribArray, GLuint index)                                                                                        \
-    GL_FUNC_SIGNATURE(void, glUseProgram, GLuint program)                                                                                                   \
-    GL_FUNC_SIGNATURE(void, glDeleteVertexArrays, GLsizei n, const GLuint *arrays)                                                                          \
-    GL_FUNC_SIGNATURE(void, glGetProgramInfoLog, GLuint program, GLsizei maxLength, GLsizei *length, GLchar *infoLog)                                       \
-    GL_FUNC_SIGNATURE(void, glDeleteBuffers, GLsizei n, const GLuint * buffers)                                                                             \
-    GL_FUNC_SIGNATURE(void, glDeleteProgram, GLuint program)                                                                                                \
-    GL_FUNC_SIGNATURE(GLint, glGetUniformLocation, GLuint program, const GLchar *name)                                                                      \
-    GL_FUNC_SIGNATURE(void, glGenerateMipmap, GLenum target)                                                                                                \
-    GL_FUNC_SIGNATURE(void, glActiveTexture, GLenum texture)                                                                                                \
-    GL_FUNC_SIGNATURE(void, glUniform1i, GLint location, GLint v0)                                                                                          \
-    GL_FUNC_SIGNATURE(void, glUniform4f, GLint location, GLfloat v0, GLfloat v1, GLfloat v2, GLfloat v3)                                                    \
-    GL_FUNC_SIGNATURE(void, glBufferSubData, GLenum target, GLintptr offset, GLsizeiptr size, const GLvoid *data)                                           \
-    GL_FUNC_SIGNATURE(void, glDebugMessageCallback, GLDEBUGPROC callback, void *userParam)                                                                  \
-    GL_FUNC_SIGNATURE(void, glDebugMessageControl,	GLenum source, GLenum type, GLenum severity, GLsizei count, const GLuint *ids, GLboolean enabled)       \
-    GL_FUNC_SIGNATURE(void, glDebugMessageInsert, GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const char *message)              \
-    GL_FUNC_SIGNATURE(void, glVertexAttribDivisor, GLuint index, GLuint divisor)                                                                            \
-    GL_FUNC_SIGNATURE(void, glDrawElementsInstanced, GLenum mode, GLsizei count, GLenum type, const void * indices, GLsizei primcount)                      \
-    GL_FUNC_SIGNATURE(void, glBindBufferBase,	GLenum target, GLuint index, GLuint buffer)                                                                   \
-    GL_FUNC_SIGNATURE(void, glTexStorage3D, GLenum target, GLsizei levels, GLenum internalformat, GLsizei width, GLsizei height, GLsizei depth)             \
-    GL_FUNC_SIGNATURE(void, glTexSubImage3D, GLenum target, GLint level, GLint xoffset, GLint yoffset, GLint zoffset, GLsizei width, GLsizei height, GLsizei depth, GLenum format, GLenum type, const GLvoid * pixels) \
-    GL_FUNC_SIGNATURE(void, glTexImage3D, GLenum target, GLint level, GLint internalFormat, GLsizei width, GLsizei height, GLsizei depth, GLint border, GLenum format, GLenum type, const GLvoid * data) \
-
-
-    // end of list 
-REQUIRED_OPENGL_FUNCTIONS
-#undef GL_FUNC_SIGNATURE
-#define GL_FUNC_SIGNATURE(ret, name, ...) glfunc_##name * name;
-REQUIRED_OPENGL_FUNCTIONS
-#undef GL_FUNC_SIGNATURE
-bool opengl_load_functions(void)
-{
-    HMODULE module = LoadLibraryExW(L"opengl32.dll", NULL, 0 );
-    if (!module) {
-        OutputDebugStringW(L"opengl32.dll not found.\n");
-        return false;
-    }
-    #define GL_FUNC_SIGNATURE(ret, name, ...)                                                       \
-            name = (glfunc_##name *) wglGetProcAddress(#name);                                      \
-            if (                                                                                    \
-                name == 0 ||                                                                        \
-                (name == (glfunc_##name *) 0x1) ||                                                             \
-                (name == (glfunc_##name *) 0x2) ||                                                             \
-                (name == (glfunc_##name *) 0x3) ||                                                             \
-                (name == (glfunc_##name *) -1)                                                                 \
-            )                                                                                       \
-            {                                                                                       \
-                OutputDebugStringW(L"wglGetProcAddress failed to find " #name "\n");                 \
-                name = (glfunc_##name *) GetProcAddress(module, #name);                             \
-            }                                                                                       \
-            if (!name)                                                                              \
-            {                                                                                       \
-                OutputDebugStringW(L"Function " #name " couldn't be loaded from opengl32.dll\n");  \
-                return false;                                                                       \
-            }
-        REQUIRED_OPENGL_FUNCTIONS
-    #undef GL_FUNC_SIGNATURE
-    return true;
-}
-#pragma endregion
 
 
 #ifdef _DEBUG
@@ -202,98 +46,8 @@ static void APIENTRY gl_debug_callback(GLenum source, GLenum type, GLuint id, GL
 render_state render_init(platform_window *window)
 {
   render_state state = {};
+  platform_opengl_init();
 
-  glfunc_wglChoosePixelFormatARB *wglChoosePixelFormatARB = 0;
-  glfunc_wglCreateContextAttribsARB *wglCreateContextAttribsARB = 0;
-
-  HWND handle = window->handle;
-  // Use first window to load OpenGL stuff
-  {
-    HDC fakeDC = GetDC(handle);
-    PIXELFORMATDESCRIPTOR pixelFormat = {0};
-    pixelFormat.nSize = sizeof(pixelFormat);
-    pixelFormat.nVersion = 1;
-    pixelFormat.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
-    pixelFormat.iPixelType = PFD_TYPE_RGBA;
-    pixelFormat.cColorBits = 32;
-    pixelFormat.cAlphaBits = 8;
-    pixelFormat.cDepthBits = 24;
-  
-    int chosenPixelFormat = ChoosePixelFormat(fakeDC, &pixelFormat);
-    ASSERT(chosenPixelFormat != 0, "ERROR: Failed to choose pixel format descriptor.");
-
-    int isPixelFormatSet = SetPixelFormat(fakeDC, chosenPixelFormat, &pixelFormat);
-    ASSERT(isPixelFormatSet != 0, "ERROR: Failed to set the pixel format descriptor.");
-
-    HGLRC fakeRC = wglCreateContext(fakeDC);
-    ASSERT(fakeRC != 0, "ERROR: Failed to create fake rendering context.");
-
-    int isFakeCurrent = wglMakeCurrent(fakeDC, fakeRC);
-    ASSERT(isFakeCurrent != 0, "ERROR: Failed to make the OpenGL rendering context the current rendering context.");
-
-    wglChoosePixelFormatARB    = (glfunc_wglChoosePixelFormatARB*) wglGetProcAddress("wglChoosePixelFormatARB");
-    wglCreateContextAttribsARB = (glfunc_wglCreateContextAttribsARB*) wglGetProcAddress("wglCreateContextAttribsARB");
-    bool loading_failed = (wglChoosePixelFormatARB == 0 || wglCreateContextAttribsARB == 0);
-    ASSERT(loading_failed == false, "ERROR: Failed to load OpenGL functions.");
-    wglMakeCurrent(fakeDC, 0);
-    wglDeleteContext(fakeRC);
-    ReleaseDC(handle, fakeDC);
-    DestroyWindow(handle);
-    UnregisterClassA("WINDOW_CLASS", window->instance);
-    window->handle = nullptr;
-    window->instance = nullptr;
-  }
-
-  // Create the real window
-  {
-    // Re-create the window
-    window_init(window);
-    handle = window->handle;
-    // Get the device context
-    state.context = GetDC(handle);
-    ASSERT(state.context != 0, "ERROR: Failed to get device context.");
-    const int pixelAttribs[] =
-    {
-      WGL_DRAW_TO_WINDOW_ARB, GL_TRUE,
-      WGL_SUPPORT_OPENGL_ARB, GL_TRUE,
-      WGL_DOUBLE_BUFFER_ARB,  GL_TRUE,
-      WGL_SWAP_METHOD_ARB,    WGL_SWAP_COPY_ARB,
-      WGL_PIXEL_TYPE_ARB,     WGL_TYPE_RGBA_ARB,
-      WGL_ACCELERATION_ARB,   WGL_FULL_ACCELERATION_ARB,
-      WGL_COLOR_BITS_ARB,     32,
-      WGL_ALPHA_BITS_ARB,     8,
-      WGL_DEPTH_BITS_ARB,     24,
-      0 // Terminate with 0, otherwise OpenGL will throw an Error!
-    };
-    UINT numPixelFormats;
-    int pixelFormat = 0;
-    BOOL chosenPixelFormatARB = wglChoosePixelFormatARB(
-      state.context,
-      pixelAttribs,
-      0, // Float List
-      1, // Max Formats
-      &pixelFormat,
-      &numPixelFormats
-    );
-    ASSERT(chosenPixelFormatARB != 0, "ERROR: Failed to wglChoosePixelFormatARB");
-    PIXELFORMATDESCRIPTOR pixelFormatDescriptor = {0};
-    DescribePixelFormat(state.context, pixelFormat, sizeof(PIXELFORMATDESCRIPTOR), &pixelFormatDescriptor);
-    BOOL isPixelFormatSet = SetPixelFormat(state.context, pixelFormat, &pixelFormatDescriptor);
-    ASSERT(isPixelFormatSet != 0, "ERROR: Failed to set the pixel format.");
-    const int contextAttribs[] = {
-        WGL_CONTEXT_MAJOR_VERSION_ARB, 4,
-        WGL_CONTEXT_MINOR_VERSION_ARB, 3,
-        WGL_CONTEXT_PROFILE_MASK_ARB, 
-        WGL_CONTEXT_CORE_PROFILE_BIT_ARB,
-        WGL_CONTEXT_FLAGS_ARB, 
-        WGL_CONTEXT_DEBUG_BIT_ARB,
-        0 // Terminate the Array
-    };
-    HGLRC renderingContext = wglCreateContextAttribsARB(state.context, 0, contextAttribs);
-    ASSERT(renderingContext != 0, "ERROR: Failed to create rendering context.");
-    BOOL isContextSet = wglMakeCurrent(state.context, renderingContext);
-    ASSERT(isContextSet != 0, "ERROR: Failed to set the device and rendering context.");
-  }
   // Load OpenGL functions
   opengl_load_functions();
   // Enable debug function
@@ -320,13 +74,26 @@ render_state render_init(platform_window *window)
 
   glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
   // Set the viewport
-  RECT rect;
-  GetClientRect(handle, &rect);
-  glViewport(0, 0, rect.right, rect.bottom);
+  glViewport(0, 0, window->width, window->height);
   return state;
 }
 
+void frame_init(render_state *state)
+{
+  glViewport(0, 0, state->width, state->height);
+  // Clear screen
+  glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
 
+
+void frame_render()
+{
+  platform_swapbuffers();
+}
+
+
+#if 0
 render_buffer render_buffer_init(void *data, size_t length)
 {
   render_buffer buffer = {};
@@ -624,13 +391,6 @@ void buffer_cube_add()
 }
 
 
-void frame_init(render_state *state)
-{
-  glViewport(0, 0, state->width, state->height);
-  // Clear screen
-  glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-}
 
 
 void uniform_set_mat4(u32 shader_program, const char *name, const f32 *data)
@@ -749,10 +509,6 @@ void draw_wireframe_elements(render_buffer buffer, u32 shader_program, u32 amoun
 }
 
 
-void frame_render(render_state *state)
-{
-  SwapBuffers(state->context);
-}
 
 
 void shader_close(u32 shader_program)
@@ -766,3 +522,5 @@ void render_close(render_state *state)
   HWND handle = WindowFromDC(state->context);
   ReleaseDC(handle, state->context);
 }
+
+#endif
