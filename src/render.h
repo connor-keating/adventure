436c3666a1bbb7ba6964443183194a4c2ff0a98d
seaweed <retro@seaweed.com>
2025-11-02 16:58:11 -0500

    Creating a render.h file for renderer implementations.

---
+++ src/render.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "core.h"
+#include "platform.h"
+
+struct shaders;
+typedef shaders* shaders_ptr;
+
+
+void          render_init(arena *a);
+void          render_resize(i32 width, i32 height);
+shaders_ptr   render_triangle(arena *a);
+void          render_draw(shaders_ptr s);
+void          render_close();
+
+void          frame_init();
+void          frame_render();
