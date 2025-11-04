ebe129ebcbe9b0cc1eedc768a4b0659a5a03beca
seaweed <retro@seaweed.com>
2025-11-04 18:48:53 -0500

    Including d3dcompiler library during application build.

--- build.bat
+++ build.bat
@@ -33,7 +33,7 @@ set assembly=application
 set app_flags=-D_DX11 -D_DEBUG
 set compiler_flags=-g -std=c++20 -Wvarargs -Wall -Werror -Wno-unused-function -Wno-unused-variable -Wno-unused-but-set-variable -Wno-deprecated
 set includes=-I\src\ -I..\external\
-set linker_flags=-luser32 -lgdi32 -lwinmm -ld3d11 -ldxgi -lopengl32
+set linker_flags=-luser32 -lgdi32 -lwinmm -ld3d11 -ldxgi -lopengl32 -ld3dcompiler
 :: set defines=
 
 
