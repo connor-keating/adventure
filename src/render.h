7acbaf2a3d32088d5aba7530ef44b533b4a12d07
Connor <conkeating32@gmail.com>
2025-11-16 16:03:07 -0500

    Organizing functions a big and creating some constant buffer functions.

--- src/render.h
+++ src/render.h
@@ -35,13 +35,18 @@ struct vert_texture
 
 void          render_init(arena *a);
 void          render_resize(i32 width, i32 height);
+void          render_close();
+
 rbuffer_ptr   render_buffer_init(arena *a, buffer_type t, void* data, u32 stride, u32 byte_count);
 rbuffer_ptr   render_buffer_dynamic_init(arena *a, buffer_type t, void *data, u32 stride, u32 byte_count);
 void          render_buffer_update(rbuffer_ptr buffer, void* data, u32 byte_count);
+rbuffer_ptr   render_buffer_constant_init( arena *a, size_t byte_count );
+void          render_constant_set( rbuffer_ptr b );
+
 void          render_text_init(arena *a);
+
 void          render_draw(rbuffer_ptr vertex_buffer, shaders_ptr s, u32 count);
 void          render_draw_elems(rbuffer_ptr vbuffer, rbuffer_ptr ebuffer, shaders_ptr s, u32 count, u32 elem_start, u32 vert_start);
-void          render_close();
 
 texture2d_ptr texture2d_init(arena *a, void* pixels, i32 width, i32 height, i32 channels);
 void          texture2d_bind(texture2d *tex, u32 slot);
