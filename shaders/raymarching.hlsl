06f7ade78c4583c2a9372bff8df3f9af5afa57cc
Connor <conkeating32@gmail.com>
2025-11-20 10:19:20 -0500

    Starting a raymarching shader.

---
+++ shaders/raymarching.hlsl
@@ -0,0 +1,34 @@
+struct VSIn {
+  float3 pos   : POSITION;
+  float4 color : COLOR0;
+  float2 uv    : TEXCOORD0;
+};
+
+struct VSOut {
+  float4 pos   : SV_POSITION;
+  float4 color : COLOR0;
+  float2 uv    : TEXCOORD0;
+};
+
+Texture3D<float> voxelTexture : register(t0);
+SamplerState voxelSampler : register(s0);
+
+cbuffer camera : register(b0)
+{
+  float4x4 view_projection;
+};
+
+VSOut VSMain(VSIn i)
+{
+  VSOut output = {
+    mul(view_projection, float4(i.pos, 1.0f)),
+    i.color,
+    i.uv
+  };
+  return output;
+}
+
+float4 PSMain(VSOut i) : SV_Target
+{
+  return float4(i.color.rgb, i.color.a);
+}
