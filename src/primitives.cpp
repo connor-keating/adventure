869207efe1685b2f689c5f62c77888fb3b4e60c4
Connor <conkeating32@gmail.com>
2025-11-20 10:08:12 -0500

    box2d is now counter clockwise.

--- src/primitives.cpp
+++ src/primitives.cpp
@@ -6,16 +6,16 @@ void primitive_box2d( arena *vbuffer, arena *ebuffer )
   // Vertices
   f32 verts[36] = {
     // position          // color (RGBA)              // Texture
-    -1.0f, -1.0f, 0.5f,  1.0f, 1.0f, 0.0f, 1.0f,  0.0f, 1.0f,  // low left
-    -1.0f,  1.0f, 0.5f,  1.0f, 0.0f, 1.0f, 1.0f,  0.0f, 0.0f, // up  left
-     1.0f, -1.0f, 0.5f,  0.0f, 1.0f, 1.0f, 1.0f,  1.0f, 1.0f, // low right
-     1.0f,  1.0f, 0.5f,  1.0f, 1.0f, 1.0f, 1.0f,  1.0f, 0.0f, // up  right
+    -1.0f, -1.0f, 0.0f,  1.0f, 1.0f, 0.0f, 1.0f,  0.0f, 1.0f,  // low left
+     1.0f, -1.0f, 0.0f,  1.0f, 0.0f, 1.0f, 1.0f,  0.0f, 0.0f, // up  left
+     1.0f,  1.0f, 0.0f,  0.0f, 1.0f, 1.0f, 1.0f,  1.0f, 1.0f, // low right
+    -1.0f,  1.0f, 0.0f,  1.0f, 1.0f, 1.0f, 1.0f,  1.0f, 0.0f, // up  right
   };
   // Elements
   u32 elems[6] = {
     // D3D11 is counter-clockwise winding order for front.
     0, 1, 2, // tri 1
-    1, 3, 2, // tri 2
+    0, 2, 3, // tri 2
   };
   f32 *vtemp = arena_push_array( vbuffer, 36, f32 );
   u32 *etemp = arena_push_array( ebuffer,  6, u32 );
@@ -25,7 +25,7 @@ void primitive_box2d( arena *vbuffer, arena *ebuffer )
 
 void primitive_box3d( arena *vbuffer, arena *ebuffer )
 {
-  // Vertices 
+  // Vertices
   f32 verts[] = {
       // positions      // color fvec4           // tex fvec2
     -1.0f,-1.0f,-1.0f,  0.0f, 0.0f, 0.0f, 1.0f,  0.0f, 0.0f,
@@ -43,11 +43,11 @@ void primitive_box3d( arena *vbuffer, arena *ebuffer )
     4,5, 5,6, 6,7, 7,4,        // top
     0,4, 1,5, 2,6, 3,7         // verticals
   };
-  u32 vert_count = sizeof(verts) / ( sizeof(vertex1) ); 
+  u32 vert_count = sizeof(verts) / ( sizeof(vertex1) );
   u32 elem_count = ARRAY_COUNT(elems);
   vertex1 *vertices = arena_push_array(vbuffer, vert_count, vertex1);
   u32     *elements = arena_push_array(ebuffer, elem_count, u32);
-  memcpy(vertices, verts, sizeof(verts)); 
+  memcpy(vertices, verts, sizeof(verts));
   memcpy(elements, elems, sizeof(elems));
   /*
   f32 *vtemp = arena_push_array( vbuffer, 216, f32 );
@@ -55,4 +55,28 @@ void primitive_box3d( arena *vbuffer, arena *ebuffer )
   memcpy( vtemp, verts, sizeof(verts) );
   memcpy( etemp, elems, sizeof(elems) );
   */
+}
+
+void primitive_quad_fullscreen( arena *vbuffer, arena *ebuffer )
+{
+  // Fullscreen quad in NDC coordinates (-1 to 1)
+  f32 verts[] = {
+      // positions      // color fvec4           // tex fvec2
+    -1.0f, -1.0f, 0.0f,  1.0f, 1.0f, 1.0f, 1.0f,  0.0f, 0.0f,  // bottom-left
+     1.0f, -1.0f, 0.0f,  1.0f, 1.0f, 1.0f, 1.0f,  1.0f, 0.0f,  // bottom-right
+     1.0f,  1.0f, 0.0f,  1.0f, 1.0f, 1.0f, 1.0f,  1.0f, 1.0f,  // top-right
+    -1.0f,  1.0f, 0.0f,  1.0f, 1.0f, 1.0f, 1.0f,  0.0f, 1.0f,  // top-left
+  };
+  // Elements - two triangles
+  u32 elems[] = {
+    0, 1, 2,  // first triangle
+    0, 2, 3   // second triangle
+  };
+
+  u32 vert_count = 4;
+  u32 elem_count = 6;
+  vertex1 *vertices = arena_push_array(vbuffer, vert_count, vertex1);
+  u32     *elements = arena_push_array(ebuffer, elem_count, u32);
+  memcpy(vertices, verts, sizeof(verts));
+  memcpy(elements, elems, sizeof(elems));
 }
