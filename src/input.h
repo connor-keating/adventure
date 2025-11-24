aa9f84e80a1ff97ae5631ff0f255e9bcb9106141
Connor <conkeating32@gmail.com>
2025-11-24 10:32:56 -0500

    Creating header file for input handling.

---
+++ src/input.h
@@ -0,0 +1,20 @@
+#pragma once
+
+// Your application should define input_state map[KEY_COUNT];
+
+
+enum input_key
+{
+  KEY_UNKNOWN,
+  KEY_SELECT,
+  KEY_ESCAPE,
+  KEY_COUNT
+};
+
+enum input_state
+{
+  INPUT_UP,
+  INPUT_HELD,
+  INPUT_DOWN,
+  INPUT_RELEASED,
+};
