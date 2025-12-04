d67d4919ba14f0fce4c22a208dae5e9774185485
seaweed <retro@seaweed.com>
2025-12-04 16:06:36 -0500

    Inputs now lives in appstate struct. Creating, opening, and showing a window in the application layer.

--- src/raymarcher.cpp
+++ src/raymarcher.cpp
@@ -7,6 +7,7 @@ struct appstate
 {
   bool is_running;
   platform_window window;
+  input_state inputs[KEY_COUNT];
 };
 
 global appstate *state;
@@ -19,13 +20,23 @@ void app_init( arena *memory )
   state->is_running = false;
   // Start the platform layer
   platform_init( memory );
+  // Create a window for the application
+  state->window = platform_window_init();
+  platform_window_show();
 }
 
 bool app_is_running()
 {
+  state->is_running = platform_is_running();
   return state->is_running;
 }
 
 void app_update( arena *memory )
 {
+  // Empty message queue
+  platform_message_process( &state->window, state->inputs );
+  if ( state->inputs[KEY_ESCAPE] == INPUT_DOWN )
+  {
+    platform_window_close();
+  }
 }
