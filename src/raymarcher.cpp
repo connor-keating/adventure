916d6d43bd016a9324ba866ef0fda10d07192c5e
seaweed <retro@seaweed.com>
2025-12-04 15:56:51 -0500

    Beginning the implementation of my first application.

--- src/raymarcher.cpp
+++ src/raymarcher.cpp
@@ -8,3 +8,12 @@ void app_init( arena *memory )
   // Start the platform layer
   platform_init( memory );
 }
+
+bool app_is_running()
+{
+  return false;
+}
+
+void app_update( arena *memory )
+{
+}
