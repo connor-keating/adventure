16d218c0a5a9496f988b31ab7ba29ff883a20129
seaweed <retro@seaweed.com>
2025-12-04 15:59:55 -0500

    Creating internal application state struct.

--- src/raymarcher.cpp
+++ src/raymarcher.cpp
@@ -3,15 +3,27 @@
 #include "platform.h"
 
 
+struct appstate
+{
+  bool is_running;
+  platform_window window;
+};
+
+global appstate *state;
+
+
 void app_init( arena *memory )
 {
+  // Create internal global state
+  state = arena_push_struct( memory, appstate );
+  state->is_running = false;
   // Start the platform layer
   platform_init( memory );
 }
 
 bool app_is_running()
 {
-  return false;
+  return state->is_running;
 }
 
 void app_update( arena *memory )
