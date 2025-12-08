684c760dffbbb26dd2deaa86a60d5922b9e3d6ab
seaweed <retro@seaweed.com>
2025-12-08 15:39:53 -0500

    Creating temporary box2d function to load different colors.

--- src/primitives.cpp
+++ src/primitives.cpp
@@ -27,16 +27,45 @@ model3d primitive_box2d( arena *vbuffer, arena *ebuffer )
   output.elem_start = elems_loaded_count;
   output.count = elem_count; 
   // Load data into arena
-  for (i32 i = 0; i < elem_count; i++)
-  { 
-    elems[i] += output.vert_start;
-  }
   f32 *vtemp = arena_push_array( vbuffer, 36, f32 );
   u32 *etemp = arena_push_array( ebuffer,  6, u32 );
   memcpy( vtemp, verts, sizeof(verts) );
   memcpy( etemp, elems, sizeof(elems) );
   return output;
 }
+
+model3d primitive_box2d_extra( arena *vbuffer, arena *ebuffer )
+{
+  // Initialize output
+  model3d output = {};
+  // Vertices
+  f32 verts[36] = {
+    // position          // color (RGBA)              // Texture
+    -1.0f, -1.0f, 0.0f,  1.0f, 0.0f, 0.0f, 1.0f,  0.0f, 0.0f, // low left
+     1.0f, -1.0f, 0.0f,  0.0f, 1.0f, 0.0f, 1.0f,  1.0f, 0.0f, // low right
+     1.0f,  1.0f, 0.0f,  0.0f, 0.0f, 1.0f, 1.0f,  1.0f, 1.0f, // top right
+    -1.0f,  1.0f, 0.0f,  0.0f, 0.0f, 0.0f, 1.0f,  0.0f, 1.0f, // top left
+  };
+  // Elements
+  u32 elems[6] = {
+    0, 1, 2, // tri 1
+    0, 2, 3, // tri 2
+  };
+  // Get and set vertex and element starting position in arena
+  u32 elem_count = ARRAY_COUNT(elems);
+  u32 verts_loaded_count = vbuffer->offset_new / sizeof(vertex1);
+  u32 elems_loaded_count = ebuffer->offset_new / sizeof(u32);
+  output.vert_start = verts_loaded_count;
+  output.elem_start = elems_loaded_count;
+  output.count = elem_count; 
+  // Load data into arena
+  f32 *vtemp = arena_push_array( vbuffer, 36, f32 );
+  u32 *etemp = arena_push_array( ebuffer,  6, u32 );
+  memcpy( vtemp, verts, sizeof(verts) );
+  memcpy( etemp, elems, sizeof(elems) );
+  return output;
+}
+
 
 void primitive_box3d( arena *vbuffer, arena *ebuffer )
 {
