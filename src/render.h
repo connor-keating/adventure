e6ef8729611a37a25eb8336d0801ac9eedd87a2e
Connor <conkeating32@gmail.com>
2025-12-20 15:35:42 -0500

    Fixing spacing.

--- src/render.h
+++ src/render.h
@@ -44,34 +44,34 @@ struct model3d
   u64 count;
 };
 
-void          render_init(arena *a);
-void          render_data_init( arena *a, u64 shader_count );
-void          render_resize(i32 width, i32 height);
-void          render_close();
+void       render_init(arena *a);
+void       render_data_init( arena *a, u64 shader_count );
+void       render_resize(i32 width, i32 height);
+void       render_close();
 
-rbuffer*      rbuffer_init(arena *a, buffer_type t, void* data, u32 stride, u32 byte_count);
-void          rbuffer_close( rbuffer* b );
-rbuffer*      rbuffer_dynamic_init(arena *a, buffer_type t, void *data, u32 stride, u32 byte_count);
-void          rbuffer_update(rbuffer* buffer, void* data, u32 byte_count);
-void          render_constant_set( rbuffer* b, u32 slot );
+rbuffer*   rbuffer_init(arena *a, buffer_type t, void* data, u32 stride, u32 byte_count);
+void       rbuffer_close( rbuffer* b );
+rbuffer*   rbuffer_dynamic_init(arena *a, buffer_type t, void *data, u32 stride, u32 byte_count);
+void       rbuffer_update(rbuffer* buffer, void* data, u32 byte_count);
+void       render_constant_set( rbuffer* b, u32 slot );
 
-void          render_text_init(arena *a);
+void       render_text_init(arena *a);
 
-void          render_draw(rbuffer* vertex_buffer, shaders* s, u32 count);
-void          render_draw_elems(rbuffer* vbuffer, rbuffer* ebuffer, u64 shader_index, u32 count, u32 elem_start, u32 vert_start);
-void          render_draw_ui(rbuffer* vertex_buffer, shaders* s, u32 count);
-void          render_draw_ui_elems(rbuffer* vbuffer, rbuffer* ebuffer, u64 shader_index, u32 count, u32 elem_start, u32 vert_start);
+void       render_draw(rbuffer* vertex_buffer, shaders* s, u32 count);
+void       render_draw_elems(rbuffer* vbuffer, rbuffer* ebuffer, u64 shader_index, u32 count, u32 elem_start, u32 vert_start);
+void       render_draw_ui(rbuffer* vertex_buffer, shaders* s, u32 count);
+void       render_draw_ui_elems(rbuffer* vbuffer, rbuffer* ebuffer, u64 shader_index, u32 count, u32 elem_start, u32 vert_start);
 
 texture*   texture1d_init(arena *a, void* data, i32 width);
 texture*   texture2d_init(arena *a, void* pixels, i32 width, i32 height, i32 channels);
 texture*   texture3d_init(arena *a, void* data, i32 width, i32 height, i32 depth);
-void          texture_bind(texture *tex, u32 slot);
-void          texture_close(texture *tex);
+void       texture_bind(texture *tex, u32 slot);
+void       texture_close(texture *tex);
 
 
-u64           shader_init(arena *a);
-void          shader_load( u64 shader_index, shader_type t, const char *file, const char *entry, const char *target);
-void          shader_close(shaders *s);
+u64        shader_init(arena *a);
+void       shader_load( u64 shader_index, shader_type t, const char *file, const char *entry, const char *target);
+void       shader_close(shaders *s);
 
-void          frame_init();
-void          frame_render();
+void       frame_init();
+void       frame_render();
