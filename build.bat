7ff8d8848d6dc57dc5bb57422def7032e348a358
Connor <conkeating32@gmail.com>
2025-12-20 15:05:28 -0500

    Moving application source code to another folder.

--- build.bat
+++ build.bat
@@ -37,9 +37,10 @@ if %mode% == _PRODUCTION (
 pushd "src"
 
 set assembly=application
+set app_src_dir=..\apps\
 set app_flags=-D_D3D -D%mode%
 set compiler_flags=-g -std=c++20 -Wvarargs -Wall -Werror -Wno-unused-function -Wno-unused-variable -Wno-unused-but-set-variable -Wno-deprecated
-set includes=-I\src\ -I..\external\
+set includes=-I.\ -I..\external\ -I%app_src_dir%
 set linker_flags=-luser32 -lgdi32 -lwinmm -ld3d11 -ldxgi -lopengl32 -ld3dcompiler
 :: set defines=
 
@@ -49,7 +50,7 @@ set linker_flags=-luser32 -lgdi32 -lwinmm -ld3d11 -ldxgi -lopengl32 -ld3dcompile
 :: Windows platform lib
 set plat_assembly=platform
 set plat_defines=-D%mode% -D_EXPORT
-set plat_includes=-I\src\ -I..\external\
+set plat_includes=-I.\ -I..\external\
 set plat_flags_comp=-g -std=c++20 -shared -Wvarargs -Wall -Werror -Wno-deprecated -Wno-unused-function
 set plat_flags_link=-luser32 -lgdi32 -lwinmm -lopengl32
 
@@ -68,7 +69,7 @@ echo:
 :: Build application
 echo %assembly% compiling...
 :: clang++ %compiler_flags% %app_flags% maintest.cpp %code_files% -o %outdir%\%assembly%.exe %defines% %includes% %linker_flags% -L%OUTDIR% -l%plat_assembly%.lib
-clang++ %compiler_flags% %app_flags% main.cpp render_dx11.cpp raymarcher.cpp %code_files% -o %outdir%\%assembly%.exe %defines% %includes% %linker_flags% -L%OUTDIR% -l%plat_assembly%.lib
+clang++ %compiler_flags% %app_flags% main.cpp render_dx11.cpp %app_src_dir%raymarcher.cpp %code_files% -o %outdir%\%assembly%.exe %defines% %includes% %linker_flags% -L%OUTDIR% -l%plat_assembly%.lib
 
 popd 
 
