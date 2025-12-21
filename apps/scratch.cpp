5eefa5b3f3bdb64271f24831980b605d3071cc2a
Connor <conkeating32@gmail.com>
2025-12-21 10:00:28 -0500

    Drawing basic triangle in scratch app.

--- apps/scratch.cpp
+++ apps/scratch.cpp
@@ -4,12 +4,18 @@
 #include "input.h"
 #include "platform.h"
 #include "render.h"
+#include "app_data.h"
+
+#define MAX_COUNT_SHADERS 100
+#define MAX_COUNT_VERTEX  1000
+
 
 struct appstate
 {
-  bool is_running;
-  platform_window window;
-  input_state inputs[KEY_COUNT];
+  bool             is_running;
+  platform_window  window;
+  input_state      inputs[KEY_COUNT];
+  u64              shader[MAX_COUNT_SHADERS];
 };
 
 global appstate *state;
@@ -31,6 +37,12 @@ void app_init(arena *memory)
   state->window = platform_window_init();
   // Initialize renderer
   render_init(memory);
+  render_data_init( memory, MAX_COUNT_SHADERS );
+  // Shaders
+  state->shader[0] = shader_init( memory );
+  shader_load( state->shader[0], VERTEX, "shaders/tri.hlsl", "VSMain", "vs_5_0");
+  shader_load( state->shader[0], PIXEL,  "shaders/tri.hlsl", "PSMain", "ps_5_0");
+  // Start the window
   platform_window_show();
 }
 
@@ -42,8 +54,10 @@ void app_update(arena *a)
     platform_window_close();
   }
   // fvec4 frame_background = fvec4_init( 0.0f, 0.325f, 0.282f, 1.0f );
-  fvec4 frame_background = fvec4_init(0.0f, 0.0f, 0.0f, 0.0f);
+  fvec4 frame_background = fvec4_init(0.0f, 0.0f, 0.0f, 1.0f);
 
   frame_init(frame_background.array);
+  shader_set( state->shader[0] );
+  render_draw( 3 );
   frame_render();
 }
