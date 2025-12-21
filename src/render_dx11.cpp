6f0d7c616223ed22dc1860ad9863c3bc0a7e6b80
Connor <conkeating32@gmail.com>
2025-12-21 14:57:36 -0500

    Whitespace.

--- src/render_dx11.cpp
+++ src/render_dx11.cpp
@@ -127,7 +127,7 @@ internal ID3D11InputLayout * render_vertex_description(ID3DBlob *vert_shader)
 {
   D3D11_INPUT_ELEMENT_DESC il[] =
   {
-    { "POSITION", 0, DXGI_FORMAT_R32G32B32A32_FLOAT,    0, 0,  D3D11_INPUT_PER_VERTEX_DATA, 0 },
+    { "POSITION", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
   };
   /*
   D3D11_INPUT_ELEMENT_DESC il[] =
