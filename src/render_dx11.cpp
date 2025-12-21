53915ca194a8feaa5ee3ccee21a28761e1dbae19
Connor <conkeating32@gmail.com>
2025-12-21 10:45:40 -0500

    Temporary vertex description for scratch app.

--- src/render_dx11.cpp
+++ src/render_dx11.cpp
@@ -127,10 +127,16 @@ internal ID3D11InputLayout * render_vertex_description(ID3DBlob *vert_shader)
 {
   D3D11_INPUT_ELEMENT_DESC il[] =
   {
+    { "POSITION", 0, DXGI_FORMAT_R32G32B32A32_FLOAT,    0, 0,  D3D11_INPUT_PER_VERTEX_DATA, 0 },
+  };
+  /*
+  D3D11_INPUT_ELEMENT_DESC il[] =
+  {
     { "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT,    0, 0,  D3D11_INPUT_PER_VERTEX_DATA, 0 },
     { "COLOR",    0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },
     { "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT,       0, 28, D3D11_INPUT_PER_VERTEX_DATA, 0 },
   };
+  */
   ID3D11InputLayout* layout = nullptr;
   renderer->device->CreateInputLayout(
     il, 
