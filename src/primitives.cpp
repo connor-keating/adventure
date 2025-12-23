10f202d3a876d6b4e38776d4b6d0de2a9920aa3d
Connor <conkeating32@gmail.com>
2025-12-23 14:51:12 -0500

    Updating primitive function to use new format. Commenting out raymarcher vertex input layout.

--- src/primitives.cpp
+++ src/primitives.cpp
@@ -7,6 +7,14 @@ model3d primitive_box2d( arena *vbuffer, arena *ebuffer, fvec4 color )
   // Initialize output
   model3d output = {};
   // Vertices
+  f32 verts[16] = {
+    // position          // color (RGBA)              // Texture
+    -1.0f, -1.0f, 0.0f, 1.0f, 
+     1.0f, -1.0f, 0.0f, 1.0f, 
+     1.0f,  1.0f, 0.0f, 1.0f, 
+    -1.0f,  1.0f, 0.0f, 1.0f
+  };
+  /*
   f32 verts[36] = {
     // position          // color (RGBA)              // Texture
     -1.0f, -1.0f, 0.0f,  0.0f, 0.0f, 0.0f, 1.0f,  0.0f, 0.0f, // low left
@@ -37,19 +45,20 @@ model3d primitive_box2d( arena *vbuffer, arena *ebuffer, fvec4 color )
     verts[32] = color.z;
     verts[33] = color.w;
   }
+  */
   u32 elems[6] = {
     0, 1, 2, // tri 1
     0, 2, 3, // tri 2
   };
   // Get and set vertex and element starting position in arena
   u32 elem_count = ARRAY_COUNT(elems);
-  u32 verts_loaded_count = vbuffer->offset_new / sizeof(vertex1);
+  u32 verts_loaded_count = vbuffer->offset_new / sizeof(f32);
   u32 elems_loaded_count = ebuffer->offset_new / sizeof(u32);
   output.vert_start = verts_loaded_count;
   output.elem_start = elems_loaded_count;
   output.count = elem_count; 
   // Load data into arena
-  f32 *vtemp = arena_push_array( vbuffer, 36, f32 );
+  f32 *vtemp = arena_push_array( vbuffer, 16, f32 );
   u32 *etemp = arena_push_array( ebuffer,  6, u32 );
   memcpy( vtemp, verts, sizeof(verts) );
   memcpy( etemp, elems, sizeof(elems) );
