8d56a33075d62f1b05098c03cc583264603c3f15
Connor <conkeating32@gmail.com>
2025-12-23 16:09:29 -0500

    Moving previous camera definition to raymarcher.cpp.

--- apps/raymarcher.cpp
+++ apps/raymarcher.cpp
@@ -16,6 +16,14 @@
 #define MAX_COUNT_RBUFFER 100
 #define MAX_COUNT_SHADERS 100
 
+struct camera
+{
+  glm::mat4 view_inv;            // 64 bytes
+  glm::mat4 proj_inv;            // 64 bytes
+  glm::mat4 wrld_inv;            // 64 bytes - inverse rotation for volume
+  glm::vec3 pos;                 // 12 bytes
+  f32 _padding;                  // 4 bytes → pad to 16-byte multiple
+};
 
 struct appstate
 {
