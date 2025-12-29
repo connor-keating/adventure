03a04d0fe6bcf26db6e05b02c4d5264951538119
Connor <conkeating32@gmail.com>
2025-12-29 13:30:05 -0500

    Application header is now written as a library header.

--- apps/application.h
+++ apps/application.h
@@ -2,7 +2,24 @@
 
 #include "core.h"
 
+// Macros
+#ifdef PLATFORM_WINDOWS
+  #ifdef _EXPORT
+    #define LIBFUNC __declspec(dllexport)
+  #else
+    #define LIBFUNC __declspec(dllimport)
+  #endif
+#endif
 
-bool app_is_running();
-void app_init( arena *a );
-void app_update( arena *a );
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+LIBFUNC bool app_is_running();
+LIBFUNC void app_init( arena *a );
+LIBFUNC void app_update( arena *a );
+
+// Ending stuff
+#ifdef __cplusplus
+}
+#endif
