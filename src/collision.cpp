d7428ba654f903c07418567958d801d1268c909b
seaweed <retro@seaweed.com>
2026-01-02 18:37:11 -0500

    Implementing 2d point in rectangle collision check.

--- src/collision.cpp
+++ src/collision.cpp
@@ -1,5 +1,11 @@
 
 bool point_in_rect(fvec2 point, fvec2 box_pos, fvec2 box_shape)
 {
-  return false;
+  f32 x1 = box_pos.x - box_shape.x;
+  f32 y1 = box_pos.y - box_shape.y;
+  f32 x2 = box_pos.x + box_shape.x;
+  f32 y2 = box_pos.y + box_shape.y;
+  bool xcheck = (point.x >= x1) && (point.x <= x2);
+  bool ycheck = (point.y >= y1) && (point.y <= y2);
+  return (xcheck && ycheck);
 }
