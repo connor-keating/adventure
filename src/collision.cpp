0ca228ac5213256c638877e7f42bc555ae7d466f
Connor <conkeating32@gmail.com>
2026-01-10 12:38:32 -0500

    Renaming parameter in point rect collision for clarity.

--- src/collision.cpp
+++ src/collision.cpp
@@ -1,10 +1,10 @@
 
-bool point_in_rect(fvec2 point, fvec2 box_pos, fvec2 box_shape)
+bool point_in_rect(fvec2 point, fvec2 box_center, fvec2 box_shape)
 {
-  f32 x1 = box_pos.x - box_shape.x;
-  f32 y1 = box_pos.y - box_shape.y;
-  f32 x2 = box_pos.x + box_shape.x;
-  f32 y2 = box_pos.y + box_shape.y;
+  f32 x1 = box_center.x - box_shape.x;
+  f32 y1 = box_center.y - box_shape.y;
+  f32 x2 = box_center.x + box_shape.x;
+  f32 y2 = box_center.y + box_shape.y;
   bool xcheck = (point.x >= x1) && (point.x <= x2);
   bool ycheck = (point.y >= y1) && (point.y <= y2);
   return (xcheck && ycheck);
