606d18bb57147844026926cfb2b8badf3627abe5
Connor <conkeating32@gmail.com>
2026-01-11 06:14:20 -0500

    Creating new shader dedicated to UI only.

--- shaders/ui.hlsl
+++ shaders/ui.hlsl
@@ -1,40 +1,17 @@
 
-struct VSIn {
-  float3 pos   : POSITION;
-  float4 color : COLOR0;
-  float2 uv    : TEXCOORD0;
-};
+struct vertex_in  { float3 pos : POSITION0; float4 col : COLOR0; };
+struct vertex_out { float4 pos : SV_POSITION; float4 col : COLOR; };
 
-struct VSOut {
-  float4 pos   : SV_POSITION;
-  float4 color : COLOR0;
-  float2 uv    : TEXCOORD0;
-};
-
-Texture2D fontTexture : register(t0);
-SamplerState fontSampler : register(s0);
-
-
-cbuffer camera : register(b1)
-{
-  float4x4 view_projection;
-};
-
-
-VSOut VSMain(VSIn i)
-{
-  float4 pos_local = float4(i.pos, 1.0f);
-  float4 pos_global = mul(view_projection, pos_local);
-  // float4 pos_global = mul(pos_local, view_projection);
-  VSOut output = {
-    pos_global,
-    i.color,
-    i.uv
+vertex_out VSMain( vertex_in input )
+{ 
+  vertex_out output = { 
+    float4(input.pos, 1.0f),
+    input.col
   };
   return output;
 }
 
-float4 PSMain(VSOut i) : SV_Target
-{
-  return float4(i.color.rgb, i.color.a);
+float4 PSMain(vertex_out input) : SV_TARGET 
+{ 
+  return input.col;
 }
