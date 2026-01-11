ca2defd8cd14e60f36a0f4a3db008e5403fe2b77
Connor <conkeating32@gmail.com>
2026-01-11 06:48:06 -0500

    Using the view and projection matrix inside ui.hlsl.

--- shaders/ui.hlsl
+++ shaders/ui.hlsl
@@ -2,10 +2,20 @@
 struct vertex_in  { float3 pos : POSITION0; float4 col : COLOR0; };
 struct vertex_out { float4 pos : SV_POSITION; float4 col : COLOR; };
 
+cbuffer camera : register(b0)
+{
+  float4x4 view;
+  float4x4 proj;
+  float3   pos;
+  float    _pad;
+};
+
+
 vertex_out VSMain( vertex_in input )
 { 
+  float4 out_position = mul(proj, mul(view, float4(input.pos, 1.0f) ));
   vertex_out output = { 
-    float4(input.pos, 1.0f),
+    out_position,
     input.col
   };
   return output;
