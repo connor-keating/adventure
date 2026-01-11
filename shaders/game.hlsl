262d67c9be1d1712d468969fe9da35c7fa7d0c8a
Connor <conkeating32@gmail.com>
2026-01-11 08:16:55 -0500

    Copying ui.hlsl to game.hlsl.

---
+++ shaders/game.hlsl
@@ -0,0 +1,33 @@
+
+struct vertex_in  { float3 pos : POSITION0; float4 col : COLOR0; };
+struct vertex_out { float4 pos : SV_POSITION; float4 col : COLOR; };
+
+cbuffer camera : register(b0)
+{
+  float4x4 view;
+  float4x4 proj;
+  float3   pos;
+  float    _pad;
+};
+
+cbuffer transform : register(b1)
+{
+  float4x4 world;
+};
+
+
+vertex_out VSMain( vertex_in input )
+{ 
+  float4 world_position = mul(world, float4(input.pos, 1.0f));
+  float4 out_position = mul(proj, mul(view, world_position) );
+  vertex_out output = { 
+    out_position,
+    input.col
+  };
+  return output;
+}
+
+float4 PSMain(vertex_out input) : SV_TARGET 
+{ 
+  return input.col;
+}
